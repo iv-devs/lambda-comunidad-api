06bab0409dc46aa96609dcb3ddb0afc06232b797
Cristofer <cristofer-dev@users.noreply.github.com>
2017-03-25 12:22:49 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import assert from 'assert';
+import cors from 'cors'; 
 
 import { configMongo } from './config';
 import members from './routes/member';
@@ -9,6 +10,7 @@ import members from './routes/member';
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 mongoose.connect(configMongo.databaseURL, (err, db) => {
   assert.equal(err, null);
